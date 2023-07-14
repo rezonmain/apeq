1f1f169071e8720901c783352d2b6d3d12ec1d65
rezonmain <adiazgomez101@gmail.com>
2023-07-14 00:28:39 -0700

    feat(i18n): add getAvailable locales method

--- i18n.ts
+++ i18n.ts
@@ -1,5 +1,13 @@
+import fs from "fs/promises";
 import { getRequestConfig } from "next-intl/server";
 
 export default getRequestConfig(async ({ locale }) => ({
   messages: (await import(`./messages/${locale}.json`)).default,
 }));
+
+const getAvailableLocales = async () => {
+  const files = await fs.readdir("./messages");
+  return files.map((file) => file.replace(".json", ""));
+};
+
+export { getAvailableLocales };
