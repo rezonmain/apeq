6cd6d1abbc3b842470d07e088124d18cc1b63dec
rezonmain <adiazgomez101@gmail.com>
2023-07-15 11:49:57 -0700

    feat(signInTvPage): use net methods

--- app/[locale]/sign-in/tv/page.tsx
+++ app/[locale]/sign-in/tv/page.tsx
@@ -10,11 +10,8 @@ import { Net } from "@/services/Net";
 export default function SignInTv() {
   const t = useTranslations("SignInTv");
   const qr = new QRChallenge(6);
-  const localIP =
-    process.env.NODE_ENV === "development" ? new Net().getLocalIP() : undefined;
-  const logInCallback = `${localIP ? "http://" : "https://"}${
-    localIP ?? process.env.HOST
-  }${localIP ? ":3000" : ""}/sign-in?t=${qr.token}`;
+  const net = new Net();
+  const signInUrl = `${net.baseURL}/sign-in?t=${qr.token}`;
 
   return (
     <div className="w-full h-screen flex flex-wrap justify-center items-center gap-8">
@@ -22,18 +19,18 @@ export default function SignInTv() {
         <h1 className="text-4xl font-bold mb-4">{t("title")}</h1>
         <p className="text-lg leading-8">
           {t.rich("subtitle", {
-            url: `${localIP ?? process.env.HOST}/sign-in`,
+            url: `${net.baseURL}/sign-in`,
             token: qr.token,
             code: (chunks) => <Code>{chunks}</Code>,
           })}
         </p>
         <GoBackButton href="/get-started" />
-        <Link href={logInCallback} target="_blank" rel="noreferrer noopener">
+        <Link href={signInUrl} target="_blank" rel="noreferrer noopener">
           Clickable
         </Link>
       </div>
       <div className="p-4 bg-white rounded-lg">
-        <QRCodeSVG value={logInCallback} size={240} />
+        <QRCodeSVG value={signInUrl} size={240} />
       </div>
       <QRWebsocket sub token={qr.token} />
     </div>
