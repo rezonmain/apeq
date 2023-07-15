75d2f6f801f30df927eb2c4d769fe70cad061d2f
rezonmain <adiazgomez101@gmail.com>
2023-07-15 11:49:43 -0700

    feat(Net): update methods

--- services/Net.ts
+++ services/Net.ts
@@ -3,9 +3,19 @@ import { networkInterfaces } from "os";
 class Net {
   constructor() {}
 
-  getLocalIP() {
+  get localIP() {
+    if (process.env.NODE_ENV !== "development")
+      throw new Error("This method is only available in development mode");
     const interfaces = networkInterfaces();
     return get(interfaces, "en0[1].address");
+  }
+
+  get baseURL() {
+    if (process.env.NODE_ENV === "development") {
+      return `http://${this.localIP}:3000`;
+    } else {
+      return `https://${process.env.VERCEL_URL}`;
+    }
   }
 }
 
