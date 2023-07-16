bc3ae039b8deb050bd1fadc8a0f5bf47d713d3b3
rezonmain <adiazgomez101@gmail.com>
2023-07-15 17:14:51 -0700

    fix(ClientSignIn): component style

--- components/ClientSignIn/ClientSignIn.tsx
+++ components/ClientSignIn/ClientSignIn.tsx
@@ -1,14 +1,27 @@
 "use client";
-import { SignIn } from "@clerk/nextjs";
-import QRWebsocket from "../QRWebsocket/QRWebsocket";
+import { SignIn, useAuth, useSignIn } from "@clerk/nextjs";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import SignInSkeleton from "../SignInSkeleton/SignInSkeleton";
 
 export default function ClientSignIn({ t }: { t: string | undefined }) {
+  const { isSignedIn } = useAuth();
+  const { isLoaded } = useSignIn();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isSignedIn && t) router.push(`/transport-session?t=${t}`);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSignedIn, t]);
+
   return (
     <>
-      <div>
-        <SignIn />
+      <div className="flex flex-row ">
+        {!isLoaded && <SignInSkeleton />}
+        {!isSignedIn && (
+          <SignIn afterSignInUrl={t ? `/transport-session?t=${t}` : "/"} />
+        )}
       </div>
-      {t && <QRWebsocket pub token={t} />}
     </>
   );
 }
