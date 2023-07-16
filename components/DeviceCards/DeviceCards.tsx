2ed39a072073e3cf3c58056b1590fbfbfc704514
rezonmain <adiazgomez101@gmail.com>
2023-07-15 19:09:00 -0700

    feat(DeviceCards): correct deviceIs type

--- components/DeviceCards/DeviceCards.tsx
+++ components/DeviceCards/DeviceCards.tsx
@@ -6,11 +6,11 @@ export default function DeviceCards() {
 
   return (
     <div className="flex justify-center flex-wrap gap-8">
-      <DeviceCard description={t("player")} href="/sign-in/tv" type="player" />
+      <DeviceCard description={t("player")} href="/sign-in/tv" type="play" />
       <DeviceCard
         description={t("controller")}
         href="/sign-in"
-        type="controller"
+        type="control"
       />
     </div>
   );
