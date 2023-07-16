ec3ae34e956e858bc9aa5009b9a13ec8618e7713
rezonmain <adiazgomez101@gmail.com>
2023-07-15 19:08:32 -0700

    feat(DeviceCard): update to use store service

--- components/DeviceCards/DeviceCard.tsx
+++ components/DeviceCards/DeviceCard.tsx
@@ -1,17 +1,20 @@
 "use client";
+import { useLocalStore } from "@/hooks/useLocalStore";
+import { AppLocalStore } from "@/types/AppLocalStore";
 import Link from "next-intl/link";
 
-interface DeviceCardProps {
+export interface DeviceCardProps {
   description: string;
   href: string;
-  type: "player" | "controller";
+  type: AppLocalStore["deviceIs"];
 }
 
 const DeviceCard = ({ description, href, type }: DeviceCardProps) => {
+  const { store } = useLocalStore();
   return (
     <Link href={href}>
       <button
-        onClick={() => localStorage.setItem("deviceIs", type)}
+        onClick={() => (store.deviceIs = type)}
         className="appearance-none flex justify-center items-center h-40 w-40 md:h-80 md:w-80 p-8 border border-gray-600 rounded-md hover:ring-4 focus:ring-4 hover:ring-blue-800 focus:ring-blue-800 focus:outline-none"
       >
         <span className="block md:text-lg">{description}</span>
