3ab7ad0a534711924b1db28addeb36ff5e530e88
rezonmain <adiazgomez101@gmail.com>
2023-07-15 19:06:24 -0700

    feat(home): home page logic

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 import PageLoad from "@/components/PageLoad/PageLoad";
-import { useAuth } from "@clerk/nextjs";
+import { useLocalStore } from "@/hooks/useLocalStore";
 import { useRouter } from "next-intl/client";
 import { useEffect } from "react";
-import { useReadLocalStorage } from "usehooks-ts";
 
 export default function Home() {
-  const { isSignedIn } = useAuth();
-  const deviceIs = useReadLocalStorage<"player" | "controller">("deviceIs");
+  const { store } = useLocalStore();
   const router = useRouter();
+
   useEffect(() => {
-    if (!isSignedIn || !deviceIs) return router.push("/get-started");
-    deviceIs === "player" ? router.push("/play") : router.push("/control");
-  }, [isSignedIn, router, deviceIs]);
+    if (!store.deviceIs || !store.uid) return router.push("/get-started");
+    return router.push(`/${store.deviceIs}`);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return <PageLoad />;
 }
