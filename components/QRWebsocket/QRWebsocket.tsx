fbe353b97188f3c8bc22186a23547c5bf1dd1a33
rezonmain <adiazgomez101@gmail.com>
2023-07-15 19:09:32 -0700

    feat(QRWebsocket): redirect on successful auth

--- components/QRWebsocket/QRWebsocket.tsx
+++ components/QRWebsocket/QRWebsocket.tsx
@@ -1,44 +1,32 @@
 "use client";
-import { Button } from "@/components/ui/Button";
+import { useLocalStore } from "@/hooks/useLocalStore";
 import { usePubSub } from "@/hooks/usePubSub";
 import { PubSubEvents } from "@/pubsub";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 type QRWebsocketProps = {
   channel: string;
 };
-type Conditional = { pub?: true; sub?: never } | { pub?: never; sub?: true };
-type Props = QRWebsocketProps & Conditional;
 
-export default function QRWebsocket({ channel, pub }: Props) {
-  return pub ? <Pub channel={channel} /> : <Sub channel={channel} />;
+export default function QRWebsocket({ channel }: QRWebsocketProps) {
+  return <Sub channel={channel} />;
 }
 
-const Pub = ({ channel }: { channel: string }) => {
-  const { pub } = usePubSub({ channel });
-  return (
-    <Button
-      variant="primary-outline"
-      onClick={() => pub["client-test"]("deeez nuuuuts")}
-    >
-      Client test
-    </Button>
-  );
-};
-
 const Sub = ({ channel }: { channel: string }) => {
+  const { store } = useLocalStore();
   const router = useRouter();
-  const [clientTest, setClientTest] = useState("");
   usePubSub({
     channel,
     sub: [
-      { e: PubSubEvents.Test, cb: setClientTest },
+      { e: PubSubEvents.Test, cb: (msg) => console.log(msg) },
       {
         e: PubSubEvents.SuccessfulAuth,
-        cb: setClientTest,
+        cb: (userId) => {
+          store.uid = userId;
+          router.push("/");
+        },
       },
     ],
   });
-  return <span>Client says: {clientTest}</span>;
+  return <></>;
 };
