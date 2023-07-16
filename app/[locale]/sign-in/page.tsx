5010cee3b7c613f79a23111425c342d9fb9698fe
rezonmain <adiazgomez101@gmail.com>
2023-07-15 17:12:36 -0700

    fix(SignInPage): style

--- app/[locale]/sign-in/page.tsx
+++ app/[locale]/sign-in/page.tsx
@@ -1,14 +1,13 @@
 import ClientSignIn from "@/components/ClientSignIn/ClientSignIn";
 import GoBackButton from "@/components/GoBackButton/GoBackButton";
-
 export default function SignInPage({
   searchParams,
 }: {
   searchParams: { t: string };
 }) {
   return (
-    <article className="w-full h-screen flex flex-col justify-center items-center">
-      <div>
+    <article className="h-screen w-[400px] mx-auto flex items-center flex-col justify-center">
+      <div className="self-start mb-5">
         <GoBackButton href="/get-started" />
       </div>
       <ClientSignIn t={searchParams.t} />
