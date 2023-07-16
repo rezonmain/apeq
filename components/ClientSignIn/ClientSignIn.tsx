c3990c44cfed1b8418c66ebdaafcb700c1adcf35
rezonmain <adiazgomez101@gmail.com>
2023-07-15 19:08:16 -0700

    feat(ClientSignIn): user auth and t state logic

--- components/ClientSignIn/ClientSignIn.tsx
+++ components/ClientSignIn/ClientSignIn.tsx
@@ -3,16 +3,25 @@ import { SignIn, useAuth, useSignIn } from "@clerk/nextjs";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import SignInSkeleton from "../SignInSkeleton/SignInSkeleton";
+import { useLocalStore } from "@/hooks/useLocalStore";
 
 export default function ClientSignIn({ t }: { t: string | undefined }) {
-  const { isSignedIn } = useAuth();
+  const { isSignedIn, isLoaded: isLoadedAuth } = useAuth();
+  const { store } = useLocalStore();
   const { isLoaded } = useSignIn();
   const router = useRouter();
 
   useEffect(() => {
-    if (isSignedIn && t) router.push(`/transport-session?t=${t}`);
+    // Wait for the auth object to load
+    if (!isLoadedAuth) return;
+    // If the user arrived here with a t param, they are a control device
+    if (t) store.deviceIs = "control";
+    // If the user is signed in and has a t param, redirect them transport-session
+    if (isSignedIn && t) return router.push(`/transport-session?t=${t}`);
+    // If the user is signed but no t params provided just redirected them to the home page
+    if (isSignedIn) return router.push("/");
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isSignedIn, t]);
+  }, [isSignedIn, t, isLoadedAuth]);
 
   return (
     <>
